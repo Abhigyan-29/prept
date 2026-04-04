5cf0aec30afd2af3e579193d6b404535a8cfea6e
Abhigyan Prakash <prakashabhigyan2002@gmail.com>
2026-04-04 19:18:54 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -46,7 +46,8 @@ export default function RootLayout({ children }) {
             <Toaster richColors />
 
             <footer className="relative z-10 border-t border-white/7 py-12  mx-auto px-6 flex flex-wrap items-center justify-center text-stone-400">
-             Fully Vibe-Coded by Abhigyan 🔥
+             Skill heavy. Vibe deadly. (80:20)
+<p>Abhigyan</p>
             </footer>
           </ThemeProvider>
         </body>
